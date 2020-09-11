5464c729da8a77562cb026b75a4dede86691c4cb
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-11 04:31:43 +0000

    first commit

--- src/main/java/ratz/restful/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/ratz/restful/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,6 +3,8 @@ package ratz.restful.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//lets go
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
