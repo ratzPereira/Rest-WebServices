f64d4aa40bb6b5aa4e0d5e66d1145856b41a473f
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-14 15:39:38 +0000

    Implemented HATEOAS on retriveUsers

--- src/main/java/ratz/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/ratz/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,12 @@ package ratz.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 
 //lets go
@@ -10,6 +16,23 @@ public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource(){
+
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return  messageSource;
 	}
 
 }
