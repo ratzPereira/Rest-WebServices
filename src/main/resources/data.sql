fc07b6a0c876f7ae248030c6b775ebfcfb91e58d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-16 17:31:58 +0000

    Implemented Post Entity and relation manytoone with Users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values (1000, sysdate(), 'Ratz');
 insert into user values (2000, sysdate(), 'Carol');
 insert into user values (3000, sysdate(), 'Nasus');
+insert into post  values (1100, 'My first post', 1000);
+insert into post  values (1101, 'My first post', 1000);
