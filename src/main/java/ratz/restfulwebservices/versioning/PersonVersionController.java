b76cc7db4753f2e13bd42cfea9b2729be808be1a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-16 15:26:20 +0000

    Small example of versioning with URI's

---
+++ src/main/java/ratz/restfulwebservices/versioning/PersonVersionController.java
@@ -0,0 +1,23 @@
+package ratz.restfulwebservices.versioning;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersionController {
+
+
+    @GetMapping("v1/person")
+    public PersonV1 personV1() {
+
+        return new PersonV1("Bob Marley");
+    }
+
+    @GetMapping("v2/person")
+    public PersonV2 personV2() {
+
+        return new PersonV2(new Name("Bob", "Marley"));
+    }
+
+}
